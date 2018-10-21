ca1f9b5e04547ec1c4d78eb39f83e367246b56d2
Federico Jensen Carrasco <federicodaniel25@gmail.com>
2018-10-21 16:42:01 -0400

    Update Smoke/smokeSocket/smokeSocket.ino

--- Smoke/smokeSocket/smokeSocket.ino
+++ Smoke/smokeSocket/smokeSocket.ino
@@ -1,7 +1,5 @@
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <SocketIoClient.h>
-
-//needed for library
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
@@ -9,7 +7,7 @@
 #define ID "Smoke-"
 String kitID = ID + String(ESP.getChipId(), HEX) + String(ESP.getFlashChipId(), HEX);
 String url = "quiet-journey-37928.herokuapp.com";
-  
+
 SocketIoClient webSocket;
 
 int alarmPin = D8;
@@ -30,7 +28,7 @@ void alertTrue(const char * payload, size_t length) {
 void alertFalse(const char * payload, size_t length) {
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.printf("got alert: %s\n", payload);
-  digitalWrite(alarmPin,LOW);
+  digitalWrite(alarmPin, LOW);
 }
 
 //recibe desde la app que hay que apagar la alerta
@@ -40,7 +38,7 @@ void desactivate(const char * payload, size_t length) {
 }
 
 //Envia id del kit cuando logra conectarse al servidor
-void connectReady(const char * payload, size_t length){
+void connectReady(const char * payload, size_t length) {
   socketConnected = true;
   webSocket.emit("loginsensorkit", "{\"kitID\":\"k1000\"}");
   //webSocket.emit("loginsensorkit", "{\"kitID\":\"" + kitID + "\"}");
@@ -48,14 +46,14 @@ void connectReady(const char * payload, size_t length){
 }
 
 //Envia la alerta cuando se detecta mucho monoxido
-void sendAlert(){
-  webSocket.emit("alertfromsensor", 
-          "{\"k1000\": {\"kitName\": \"Nombre kit 1\",\"kitStatus\": \"mal\",\"sensor\": {\"k1000s1\": {\"nombre\": \"Sensor 1 del  kit 1\",\"status\": \"mal\"},\"k1000s2\": {\"nombre\": \"Sensor 2 del kit 1\",\"status\": \"bien\"}}}}");
+void sendAlert() {
+  webSocket.emit("alertfromsensor",
+                 "{\"k1000\": {\"kitName\": \"Nombre kit 1\",\"kitStatus\": \"mal\",\"sensor\": {\"k1000s1\": {\"nombre\": \"Sensor 1 del  kit 1\",\"status\": \"mal\"},\"k1000s2\": {\"nombre\": \"Sensor 2 del kit 1\",\"status\": \"bien\"}}}}");
 }
 
 //Lee la señal analogica
 void readAnalogSensor() {
-  if( millis() % 100 != 0 ) return;
+  if ( millis() % 100 != 0 ) return;
   sensorValue = analogRead(A0);
   Serial.println(sensorValue);
 }
@@ -65,30 +63,30 @@ void setup() {
   pinMode(alarmPin, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(alarmPin, LOW);
-  
+
   Serial.begin(115200);
-  
+
   WiFiManager wifiManager;
-  
+
   //reset saved settings
   //wifiManager.resetSettings();
-  
+
   //set custom ip for portal
-  wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  wifiManager.setAPStaticIPConfig(IPAddress(10, 0, 1, 1), IPAddress(10, 0, 1, 1), IPAddress(255, 255, 255, 0));
 
   wifiManager.autoConnect("Smoke");
   Serial.println("connected to WiFi");
-  delay(100);  
+  delay(100);
   Serial.println("Tratando conectar socket...");
 
   //Escucha desde el servidor eventos
   webSocket.on("responsefromservertrue", alertTrue);
   webSocket.on("responsefromserverfalse", alertFalse);
   webSocket.on("desactivate", desactivate);
-  
+
   //emitId se ejecuta solo cuando logra conectarse de manera exitosa al servidor
   webSocket.on("connect", connectReady);
-  
+
   webSocket.begin(url.c_str());
 
 }
@@ -96,7 +94,7 @@ void setup() {
 void loop() {
   //Permite mantener activo socket.io
   webSocket.loop();
-  if(socketConnected){
+  if (socketConnected) {
     readAnalogSensor();
   }
   //digitalWrite(alarmPin,LOW);
@@ -110,7 +108,7 @@ void loop() {
       enviar = false;
     }
     digitalWrite(LED_BUILTIN, LOW);
-  }*/
+    }*/
   //se abre el candado cuando el valor baja mas del limite
   //para evitar emitir la alerta mas de una vez
   /*else if(sensorValue <= 700){
@@ -119,6 +117,6 @@ void loop() {
       enviar = true;
     }
     digitalWrite(LED_BUILTIN, HIGH);
-  }*/
-    
+    }*/
+
 }
